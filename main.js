58dfd8a72fbe5a13362d20230b52ee238e783ca9
Federico Knüssel <fknussel@gmail.com>
2016-05-26 00:47:57 +1000

    Add basic game logic

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ var mainState = {
 
     // Load the bird sprite
     game.load.image('bird', 'assets/bird.png');
+
+    // Load the pipe sprite
+    game.load.image('pipe', 'assets/pipe.png');
   },
 
   create: function() {
@@ -31,6 +34,16 @@ var mainState = {
     // Call the 'jump' function when the spacekey is hit
     var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     spaceKey.onDown.add(this.jump, this);
+
+    // Create an empty group to hold the pipes
+    this.pipes = game.add.group();
+
+    // To actually add pipes in our game we need to call the addRowOfPipes() function every 1.5 seconds.
+    this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+
+    // display the score on the upper left corner
+    this.score = 0;
+    this.labelScore = game.add.text(20, 20, "0", { font: "30px Arial", fill: "#fff" });
   },
 
   update: function() {
@@ -42,6 +55,9 @@ var mainState = {
     if (this.bird.y < 0 || this.bird.y > 490) {
       this.restartGame();
     }
+
+    // also restart game on collision detection
+    game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this);
   },
 
   // Make the bird jump
@@ -54,6 +70,42 @@ var mainState = {
   restartGame: function() {
     // Start the 'main' state, which restarts the game
     game.state.start('main');
+  },
+
+  addOnePipe: function(x, y) {
+    // Create a pipe at the position x and y
+    var pipe = game.add.sprite(x, y, 'pipe');
+
+    // Add the pipe to our previously created group
+    this.pipes.add(pipe);
+
+    // Enable physics on the pipe
+    game.physics.arcade.enable(pipe);
+
+    // Add velocity to the pipe to make it move left
+    pipe.body.velocity.x = -200;
+
+    // Automatically kill the pipe when it's no longer visible
+    pipe.checkWorldBounds = true;
+    pipe.outOfBoundsKill = true;
+  },
+
+  addRowOfPipes: function() {
+    // increase the score by 1 every time a new row of pipes gets created
+    this.score++;
+    this.labelScore.text = this.score;
+
+    // Randomly pick a number between 1 and 5
+    // This will be the hole position
+    var hole = Math.floor(Math.random() * 5) + 1;
+
+    // Add the 6 pipes
+    // With one big hole at position 'hole' and 'hole + 1'
+    for (var i = 0; i < 8; i++) {
+      if (i != hole && i != hole + 1) {
+        this.addOnePipe(400, i * 60 + 10);
+      }
+    }
   },
 };
 
