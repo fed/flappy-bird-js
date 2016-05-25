edd82cf4c7cdc47cb562f93da498db8e45c33452
Federico Knüssel <fknussel@gmail.com>
2016-05-26 00:57:01 +1000

    Add visual enhancements

--- main.js
+++ main.js
@@ -44,6 +44,9 @@ var mainState = {
     // display the score on the upper left corner
     this.score = 0;
     this.labelScore = game.add.text(20, 20, "0", { font: "30px Arial", fill: "#fff" });
+
+    // Move the anchor to the left and downward
+    this.bird.anchor.setTo(-0.2, 0.5);
   },
 
   update: function() {
@@ -57,13 +60,30 @@ var mainState = {
     }
 
     // also restart game on collision detection
-    game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this);
+    game.physics.arcade.overlap(this.bird, this.pipes, this.hitPipe, null, this);
+
+    if (this.bird.angle < 20) {
+      this.bird.angle += 1;
+    }
   },
 
   // Make the bird jump
   jump: function() {
     // Add a vertical velocity to the bird
     this.bird.body.velocity.y = -350;
+
+    // Create an animation on the bird
+    var animation = game.add.tween(this.bird);
+
+    // Change the angle of the bird to -20° in 100 milliseconds
+    animation.to({angle: -20}, 100);
+
+    // And start the animation
+    animation.start();
+
+    if (!this.bird.alive) {
+      return;
+    }
   },
 
   // Restart the game
@@ -106,6 +126,25 @@ var mainState = {
         this.addOnePipe(400, i * 60 + 10);
       }
     }
+  },
+
+  hitPipe: function() {
+    // If the bird has already hit a pipe, do nothing
+    // It means the bird is already falling off the screen
+    if (!this.bird.alive) {
+      return;
+    }
+
+    // Set the alive property of the bird to false
+    this.bird.alive = false;
+
+    // Prevent new pipes from appearing
+    game.time.events.remove(this.timer);
+
+    // Go through all the pipes, and stop their movement
+    this.pipes.forEach(function(p){
+      p.body.velocity.x = 0;
+    }, this);
   },
 };
 
